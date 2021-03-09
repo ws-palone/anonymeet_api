61102aca2db6366a5d94bd9d946be8cafb6a95ad
ws-palone <ws.palone@gmail.com>
2021-03-09 22:32:57 +0100

    add put mapping for latitude and longitude

--- src/main/java/com/example/anonymeet_api/controllers/ProfileController.java
+++ src/main/java/com/example/anonymeet_api/controllers/ProfileController.java
@@ -3,12 +3,16 @@ package com.example.anonymeet_api.controllers;
 import com.example.anonymeet_api.models.Profile;
 import com.example.anonymeet_api.repositories.ProfileRepository;
 import com.example.anonymeet_api.services.ProfileService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.PropertyAccessor;
+import org.springframework.beans.PropertyAccessorFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path= "/profile")
@@ -50,6 +54,30 @@ public class ProfileController {
     public ResponseEntity<String> saveProfile(@RequestBody Profile profile){
         profileRepository.save(profile);
         return new ResponseEntity<>("Profile " + profile.getPseudo() + " successfully saved. ", HttpStatus.OK);
+    }
+
+    //@PUT MAPPING
+    @PutMapping(value = "/updateProfile")
+    public ResponseEntity<String> updateProfile(
+            @RequestParam(required=false) Map<String,String> allParams,
+            @RequestParam Long id
+    ) throws JsonProcessingException {
+        Profile profile;
+
+        profile = profileRepository.findProfileById(id);
+
+        System.out.println("Profile updated : " + profile.getId());
+
+        for (String key : allParams.keySet()) {
+            if (key.matches("latitude") || key.matches("longitude")) {
+                PropertyAccessor myAccessor = PropertyAccessorFactory.forDirectFieldAccess(profile);
+                myAccessor.setPropertyValue(key, allParams.get(key));
+            }
+        }
+
+        profileRepository.save(profile);
+
+        return new ResponseEntity<>("Profile n°" + id + " has been updated",HttpStatus.OK);
     }
 
 }
